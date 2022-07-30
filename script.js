170a333f6d747e5fefa460c257b64da3019be182
dogukan-sys <Karademird67@gmail.com>
2022-07-30 11:30:13 +0200

    Get Player Choice function

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function getComputerChoice() {
-  const choice = ['Rock','Paper','Scissor']
+  var choice = ['Rock','Paper','Scissor']
   return choice[~~(Math.random() * choice.length)]
 }
 
+function getPlayerChoice() {
+  return prompt('Choose: Rock, Paper or Scissor')
+} 
