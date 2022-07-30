57c1765d8e8d0364968fd4898db426f0bf0d4a3a
dogukan-sys <Karademird67@gmail.com>
2022-07-30 11:47:19 +0200

    create Game logic

--- script.js
+++ script.js
@@ -4,5 +4,40 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-  return prompt('Choose: Rock, Paper or Scissor')
+  var choice = prompt('Choose: Rock, Paper or Scissor')
+  if 
 } 
+
+function checkWinner(choice1, choice2) {
+  if (choice1 == 'Rock' && choice2 == 'Rock') {
+    result = 'It\'s a Tie!'
+  } else if (choice1 == 'Rock' && choice2 == 'Paper') {
+    result = 'Player Wins!'
+  } else if (choice1 == 'Rock' && choice2 == 'Scissor') {
+    result = 'Computer Wins!'
+  } else if (choice1 == 'Paper' && choice2 == 'Paper') {
+    result = 'It\'s a Tie!'
+  } else if (choice1 == 'Paper' && choice2 == 'Scissor') {
+    result = 'Player Wins!'
+  } else if (choice1 == 'Paper' && choice2 == 'Rock') {
+    result = 'Computer Wins!'
+  } else if (choice1 == 'Scissor' && choice2 == 'Scissor') {
+    result = 'It\'s a Tie!'
+  } else if (choice1 == 'Scissor' && choice2 == 'Rock') {
+    result = 'Player Wins!'
+  } else if (choice1 == 'Scissor' && choice2 == 'Paper') {
+    result = 'Computer Wins!'
+  }
+  return `You chose ${choice2} and the Computer chose ${choice1}, ${result}`
+}
+
+function game() {
+  var comp = getComputerChoice()
+  var player = getPlayerChoice()
+  alert(checkWinner(comp,player))
+  
+}
+
+while (true) {
+  game()
+}
