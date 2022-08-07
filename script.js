7cdbf576499651e3d9d7074f5f2cf475eb2539c9
dogukan-sys <Karademird67@gmail.com>
2022-08-07 23:38:29 +0200

    using switch cases to check winner

--- script.js
+++ script.js
@@ -1,79 +1,61 @@
-while (true) {
-  var comp = 0 
-  var player = 0
 
-  /**
-   * Get the Computer Choice
-   * @returns rock, paper or scissor as string
-   */
-  function getComputerChoice() {
-    var choice = ['rock','paper','scissor']
-    return choice[~~(Math.random() * choice.length)]
-  }
-  /**
-   * Gets the Players Choice with a Prompt
-   * @returns rock, paper or scissor as string
-   */
-  function getPlayerChoice() {
-    var choice = prompt('Choose: Rock, Paper or Scissor')  
-    return choice.toLowerCase()
-  } 
-
-  /**
-   * Check who won the round
-   * @param {Computer Choice} choice1 
-   * @param {Players Choice} choice2 
-   * @returns The result
-   */
-  function checkWinner(choice1, choice2) {
-    if (choice1 == 'rock' && choice2 == 'rock') {
-      result = 'It\'s a Tie!'
-    } else if (choice1 == 'rock' && choice2 == 'paper') {
-      result = 'Player Wins, this round!'
-      player++
-    } else if (choice1 == 'rock' && choice2 == 'scissor') {
-      result = 'Computer Wins, this round!'
-      comp++
-    } else if (choice1 == 'paper' && choice2 == 'paper') {
-      result = 'It\'s a Tie, this round!'
-    } else if (choice1 == 'paper' && choice2 == 'scissor') {
-      result = 'Player Wins, this round!'
-      player++
-    } else if (choice1 == 'paper' && choice2 == 'rock') {
-      result = 'Computer Wins, this round!'
-      comp++
-    } else if (choice1 == 'scissor' && choice2 == 'scissor') {
-      result = 'It\'s a Tie, this round!'
-    } else if (choice1 == 'scissor' && choice2 == 'rock') {
-      result = 'Player Wins, this round!'
-      player++
-    } else if (choice1 == 'scissor' && choice2 == 'paper') {
-      result = 'Computer Wins, this round!'
-      comp++
-    // If the players does not give a Valid Input it tells the Player
-    } else return 'Wrong input, please type rock paper or scissor'
-    return `You chose ${choice2} and the Computer chose ${choice1}, ${result}`
-  }
-
-  // main function that calls the other functions
-  function main() {
-    var computerSelection = getComputerChoice()
-    var playerSelection = getPlayerChoice()
-    alert(checkWinner(computerSelection,playerSelection))  
-  }
-  /**
-   * Checks who has a higher score
-   * @returns Winner 
-   */
-  function announceWinner() {
-    if (player > comp) {
-      return 'You Win!'
-    } else return 'Computer Wins!'
-  }
-
-  for (i=0;i<5;i++){
-    main()
-  }
-
-  alert(announceWinner())
+var comp = 0 
+var player = 0
+/**
+ * Get the Computer Choice
+ * @returns rock, paper or scissor as string
+ */
+function getComputerChoice() {
+  var choice = ['rock','paper','scissor']
+  return choice[~~(Math.random() * choice.length)]
 }
+ 
+/**
+ * Check who won the round
+ * @param {Computer Choice} choice1 
+ * @param {Players Choice} choice2 
+ * @returns The result
+ */
+function checkWinner(choice1, choice2) {
+  if (choice1 == choice2){
+    return
+  }
+  switch(choice2){
+    case "rock":
+      if (choice1 == 'paper'){
+        comp++
+      } else player++
+      break
+    case "paper":
+      if (choice1 == 'scissor'){
+        comp++
+      } else player++
+      break
+    case "scissor":
+      if (choice1 == 'rock'){
+        comp++
+      } else player++
+      break
+  }
+
+}
+// main function that calls the other functions
+function playRound(playerChoice) {
+  var computerSelection = getComputerChoice()
+  var playerSelection = playerChoice
+  alert(checkWinner(computerSelection,playerSelection))  
+}
+/**
+ * Checks who has a higher score
+ * @returns Winner 
+ */
+function announceWinner() {
+  if (player > comp) {
+    return 'You Win!'
+  } else return 'Computer Wins!'
+}
+
+const buttons = document.querySelectorAll('button')
+buttons.forEach(button => button.addEventListener('click', () =>{
+  playRound(button.id)
+}))
