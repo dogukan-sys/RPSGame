0012ec9b08009a9fe483edfcb21ac9f96b81111e
dogukan-sys <Karademird67@gmail.com>
2022-08-08 08:10:22 +0200

    update scoreboard and game over function

--- script.js
+++ script.js
@@ -1,72 +1,126 @@
-
 var comp = 0 
 var player = 0
-/**
- * Get the Computer Choice
- * @returns rock, paper or scissor as string
- */
-function getComputerChoice() {
-  var choice = ['rock','paper','scissor']
-  return choice[~~(Math.random() * choice.length)]
-}
- 
-/**
- * Check who won the round
- * @param {Computer Choice} choice1 
- * @param {Players Choice} choice2 
- * @returns The result
- */
-function checkWinner(choice1, choice2) {
+var winner = ""
+
+function playRound(choice1, choice2) {
   if (choice1 == choice2){
+    winner = 'tie'
+    updateScoreboard(choice1, choice2)
     return
   }
   switch(choice2){
     case "rock":
       if (choice1 == 'paper'){
         comp++
-      } else player++
-      compScore.innerText = `${comp}`
-      playerScore.innerText = `${player}`
+        winner = 'computer'
+      } else player++, winner = 'player'
       break
     case "paper":
       if (choice1 == 'scissor'){
         comp++
-      } else player++
-      compScore.innerText = `${comp}`
-      playerScore.innerText = `${player}`
+        winner = 'computer'
+      } else player++, winner = 'player'
       break
     case "scissor":
       if (choice1 == 'rock'){
         comp++
-      } else player++
-      compScore.innerText = `${comp}`
-      playerScore.innerText = `${player}`
+        winner = 'computer'
+      } else player++, winner = 'player'
       break
+  }
+  updateScoreboard(choice1, choice2)
+
+}
+
+function updateScoreboard(computerchoice,playerchoice){
+  playerScore.textContent = `Player: ${player}`
+  compScore.textContent = `Computer: ${comp}`
+  
+  switch (winner) {
+    case 'tie':
+      scoreInfo.textContent = 'Its a Tie!'
+      if (computerchoice == 'rock') {
+        scoreMessage.textContent = 'Rock ties with rock.'
+      } else if (computerchoice == 'paper'){
+        scoreMessage.textContent = 'Paper ties with paper.'
+      } else {
+        scoreMessage.textContent = 'Scissor ties with scissor.'
+      }
+      break;
+
+    case 'player':
+      scoreInfo.textContent = 'You Win!'
+      if (computerchoice == 'rock') {
+        scoreMessage.textContent = 'Paper beats rock.'
+      } else if (computerchoice == 'paper'){
+        scoreMessage.textContent = 'Scissor is beats paper.'
+      } else {
+        scoreMessage.textContent = 'Rock beats scissor.'
+      }
+      break;
+    case 'computer':
+      scoreInfo.textContent = 'You Lose!'
+      if (computerchoice == 'rock') {
+        scoreMessage.textContent = 'Scissor is beaten by rock.'
+      } else if (computerchoice == 'paper'){
+        scoreMessage.textContent = 'Rock is beaten by paper.'
+      } else {
+        scoreMessage.textContent = 'Paper is beaten by scissor.'
+      }
+      break;
+  }
+
+
+  switch (computerchoice) {
+    case 'rock':
+      commpSign.textContent = "✊"
+      break;
+    case 'paper':
+      commpSign.textContent = "✋"
+      break;
+    case 'scissor':
+      commpSign.textContent = "✌"
+  }
+  switch (playerchoice) {
+    case 'rock':
+      playerSign.textContent = "✊"
+      break;
+    case 'paper':
+      playerSign.textContent = "✋"
+      break;
+    case 'scissor':
+      playerSign.textContent = "✌"
   }
 
 }
-// main function that calls the other functions
-function playRound(playerChoice) {
-  var computerSelection = getComputerChoice()
-  var playerSelection = playerChoice
-  checkWinner(computerSelection,playerSelection)
+
+
+function getComputerChoice() {
+  var choice = ['rock','paper','scissor']
+  return choice[~~(Math.random() * choice.length)]
 }
-/**
- * Checks who has a higher score
- * @returns Winner 
- */
-function announceWinner() {
-  if(compScore.innerText == 5 || playerScore.innerText == 5){
-    comp = 0
-    player = 0
-    if (player > comp) { alert('You Win!') 
-    } else alert('Computer Wins!')
-  }
+
+function isGameOver(){
+  return player === 5 || comp === 5
 }
+
+const scoreInfo = document.querySelector('.score-info')
+const scoreMessage = document.querySelector('.score-message')
 const compScore = document.getElementById('compScore')
 const playerScore = document.getElementById('playerScore')
 const buttons = document.querySelectorAll('button')
+const playerSign = document.getElementById('playerSign')
+const commpSign = document.getElementById('compSign')
+
+
 buttons.forEach(button => button.addEventListener('click', () =>{
-  playRound(button.id)
-  announceWinner()
+  const computerSelection = getComputerChoice()
+  const playerSelection = button.id
+  if (isGameOver()){
+    return
+  }
+  playRound(computerSelection, playerSelection)
+  
 }))
+
+
